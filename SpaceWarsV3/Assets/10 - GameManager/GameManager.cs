db96d5a05e9d1f91a84cd68741aa7eacb54c8a31
upallnightcoding <upallnightcoding@gmail.com>
2024-08-30 02:44:54 -0400

    Main Menu Setup

---
+++ SpaceWarsV3/Assets/10 - GameManager/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public void Start()
+    {
+        
+    }
+
+    public void NewGameBtn()
+    {
+
+    }
+
+    public void LoadGameBtn()
+    {
+
+    }
+
+    public void QuitBtn()
+    {
+
+    }
+}
